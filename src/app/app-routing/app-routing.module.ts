5b006850dfdafbf883eeb962536d523c220ea5d8
Oscar Estrada <oestrada.3@gmail.com>
2018-11-28 10:27:25 -0600

    Primera subida

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router'
+
+import { LoginComponent } from '../inicio/iniciocomp/login/login.component';
+import { GeneralComponent } from '../general/general.component';
+import { AboutComponent } from '../inicio/iniciocomp/about/about.component';
+import { ContactComponent } from '../inicio/iniciocomp/contact/contact.component';
+import { NoticiasComponent } from '../general/generalcomp/noticias/noticias.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'general', component: GeneralComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'noticias', component: NoticiasComponent },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes, {useHash: true} )
+  ],
+  exports: [ RouterModule ],
+  declarations: []
+})
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './inicio/iniciocomp/login/login.component';
+import { AboutComponent } from './inicio/iniciocomp/about/about.component';
+import { ContactComponent } from './inicio/iniciocomp/contact/contact.component';
+import { GeneralComponent } from './general/general.component';
+import { NoticiasComponent } from './general/generalcomp/noticias/noticias.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    LoginComponent,
+    AboutComponent,
+    ContactComponent,
+    GeneralComponent,
+    NoticiasComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
