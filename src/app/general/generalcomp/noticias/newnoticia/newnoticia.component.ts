4a550e640124cf6fc262af1df934e5dedfe8fd67
Oscar Estrada <oestrada.3@gmail.com>
2018-11-30 16:04:36 -0600

    newnoticia

---
+++ src/app/general/generalcomp/noticias/newnoticia/newnoticia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newnoticia',
+  templateUrl: './newnoticia.component.html',
+  styleUrls: ['./newnoticia.component.scss']
+})
+export class NewnoticiaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
